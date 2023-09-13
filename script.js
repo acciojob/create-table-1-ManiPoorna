e762fa56ac10bbdb71ee5d74f8c4c38721f68e68
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-13 12:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const table = document.getElementById("sampleTable");
+
 function insert_Row() {
     //Write your code here
-  
-  
+	let count = 0;
+	const row = document.createElement("tr");
+	row.innerHTML = `<td>New Cell${++count}</td>
+					<td>New Cell${++count}</td>`	
+  table.prepend(row);
 }
