9f834503912d25f3d147b6c696acd682bea0683a
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-13 13:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const table = document.getElementById("sampleTable");
 
 function insert_Row() {
     //Write your code here
+	
 	let count = 0;
 	const row = document.createElement("tr");
 	row.innerHTML = `<td>New Cell${++count}</td>
